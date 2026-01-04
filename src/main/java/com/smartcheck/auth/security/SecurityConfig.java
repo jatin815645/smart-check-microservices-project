ac7c9bad1c2fcdffe55691e11aa4106efb2ce3f5
JitendraPatil <jitendra.patil@skosystems.com>
2026-01-04 19:18:21 +0530

    smart check config

--- src/main/java/com/smartcheck/auth/security/SecurityConfig.java
+++ src/main/java/com/smartcheck/auth/security/SecurityConfig.java
@@ -5,5 +5,5 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 
 @Configuration
 @EnableMethodSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
 }
